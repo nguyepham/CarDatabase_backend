f563d6330592544a8b19ffad2006c9512c5c42dd
NguyePham <nguyenminhpham24@gmail.com>
2024-05-14 13:53:09 +0700

    Added several RESTful services by leveraging Spring Data REST

--- src/main/java/nguye/cardatabase/repository/CarRepository.java
+++ src/main/java/nguye/cardatabase/repository/CarRepository.java
@@ -2,15 +2,19 @@ package nguye.cardatabase.repository;
 
 import nguye.cardatabase.model.Car;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
+@RepositoryRestResource
 public interface CarRepository extends CrudRepository<Car, Long> {
-    List<Car> findByBrand(String brand);
-    List<Car> findByColor(String color);
+    List<Car> findByBrand(@Param("brand") String brand);
+    List<Car> findByColor(@Param("color") String color);
     List<Car> findByModelYear(int year);
     List<Car> findByBrandAndModel(String brand, String model);
     List<Car> findByBrandOrColor(String brand, String color);
     List<Car> findByBrandOrderByModelYearAsc(String brand);
+    List<Car> findByColorOrderByModelYearAsc(String brand);
     List<Car> findByBrandOrderByModelYearDesc(String brand);
 }
