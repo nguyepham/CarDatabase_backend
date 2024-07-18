205caca03acd73e876d7d6ad35f7a1cb4e4e1758
nguyepham <nguyenminhpham24@gmail.com>
2024-07-18 13:43:20 +0700

    Added CORS filter

--- src/main/java/nguye/cardatabase/security/SecurityConfig.java
+++ src/main/java/nguye/cardatabase/security/SecurityConfig.java
@@ -14,6 +14,10 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.List;
 
 // These two annotations switch off the default web security configuration, and we can define our own configuration
 // in this class.
@@ -49,6 +53,18 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         return http.csrf(AbstractHttpConfigurer::disable)
+                .cors(configurer -> {
+                    CorsConfigurationSource source = request -> {
+                        CorsConfiguration config = new CorsConfiguration();
+
+                        config.setAllowedOrigins(List.of("*"));
+                        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+                        config.setAllowedHeaders(List.of("*"));
+
+                        return config;
+                    };
+                    configurer.configurationSource(source);
+                })
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.POST, "/login").permitAll()
